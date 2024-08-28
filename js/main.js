df8aea40b30d571cc3ea7086fe47d757d4cddc62
Maiko333777 <90162348+Maiko333777@users.noreply.github.com>
2024-08-28 15:32:30 +0900

    initial commit
    portfolio updated

---
+++ js/main.js
@@ -0,0 +1,136 @@
+// ハンバーガーメニュー
+// --------------------------------------------------------------
+$(function () {
+    $('.js-btn').on('click', function () {        // js-btnクラスをクリックすると、
+        $('.burger_header-nav , .btn-line').toggleClass('open'); // メニューとバーガーの線にopenクラスをつけ外しする
+    })
+});
+
+// aクリックしたらハンバーガーメニュー閉じる
+$('.burger_header-nav-item a').on('click', function () {
+    $('.burger_gnav, .btn-line').removeClass('open');
+});
+
+
+// slick
+// --------------------------------------------------------------
+$('.slider').slick({
+    customPaging: function (slider, index) {
+        return $('<button type="button" />').text(('00' + (index + 1)).slice(-2));
+    },
+    autoplay: false,//自動的に動き出すか。初期値はfalse。
+    infinite: true,//スライドをループさせるかどうか。初期値はtrue。
+    speed: 500,//スライドのスピード。初期値は300。
+    slidesToShow: 3,//スライドを画面に3枚見せる
+    slidesToScroll: 1,//1回のスクロールで1枚の写真を移動して見せる
+    prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
+    nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
+    centerMode: true,//要素を中央ぞろえにする
+    variableWidth: true,//幅の違う画像の高さを揃えて表示
+    dots: true,//下部ドットナビゲーションの表示
+});
+
+
+
+// top plan slider
+// --------------------------------------------------------------
+$('.plan-slider').slick({
+    autoplay: false,//自動的に動き出すか。初期値はfalse。
+    infinite: true,//スライドをループさせるかどうか。初期値はtrue。
+    slidesToShow: 3.5,//スライドを画面に3枚見せる
+    slidesToScroll: 1,//1回のスクロールで3枚の写真を移動して見せる
+    prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
+    nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
+    dots: true,//下部ドットナビゲーションの表示
+    responsive: [
+        {
+            breakpoint: 769,//モニターの横幅が769px以下の見せ方
+            settings: {
+                slidesToShow: 2,//スライドを画面に2枚見せる
+                slidesToScroll: 2,//1回のスクロールで2枚の写真を移動して見せる
+            }
+        },
+        {
+            breakpoint: 426,//モニターの横幅が426px以下の見せ方
+            settings: {
+                slidesToShow: 1,//スライドを画面に1枚見せる
+                slidesToScroll: 1,//1回のスクロールで1枚の写真を移動して見せる
+            }
+        }
+    ]
+});
+
+
+// To page top
+// --------------------------------------------------------------
+// #page-topをクリックした際の設定
+$('#page-top').click(function () {
+    $('body,html').animate({
+        scrollTop: 0//ページトップまでスクロール
+    }, 500);//ページトップスクロールの速さ。数字が大きいほど遅くなる
+    return false;//リンク自体の無効化
+});
+
+
+
+
+// -------------------------
+jQuery(function() {
+    var pagetop = $('#page_top');   
+    pagetop.hide();
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {  //100pxスクロールしたら表示
+            pagetop.fadeIn();
+        } else {
+            pagetop.fadeOut();
+        }
+    });
+    pagetop.click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 500); //0.5秒かけてトップへ移動
+        return false;
+    });
+  });
+
+
+//   ------------------------
+//3. 画像のモーダル
+$(".gallery").modaal({
+	type: 'image',
+	overlay_close:true,//モーダル背景クリック時に閉じるか
+	before_open:function(){// モーダルが開く前に行う動作
+		$('html').css('overflow-y','hidden');/*縦スクロールバーを出さない*/
+	},
+	after_close:function(){// モーダルが閉じた後に行う動作
+		$('html').css('overflow-y','scroll');/*縦スクロールバーを出す*/
+	}
+});
+
+
+// move
+function fadeAnime(){
+
+    // ふわっ
+  
+    $('.fadeInTrigger').each(function(){ //fadeInTriggerというクラス名が
+      var elemPos = $(this).offset().top-50;//要素より、50px上の
+      var scroll = $(window).scrollTop();
+      var windowHeight = $(window).height();
+      if (scroll >= elemPos - windowHeight){
+      $(this).addClass('fadeIn');// 画面内に入ったらfadeInというクラス名を追記
+      }else{
+      $(this).removeClass('fadeIn');// 画面外に出たらfadeInというクラス名を外す
+      }
+      });
+    }
+
+    // 画面をスクロールをしたら動かしたい場合の記述
+  $(window).scroll(function (){
+    fadeAnime();/* アニメーション用の関数を呼ぶ*/
+  });// ここまで画面をスクロールをしたら動かしたい場合の記述
+
+// 画面が読み込まれたらすぐに動かしたい場合の記述
+  $(window).on('load', function(){
+    fadeAnime();/* アニメーション用の関数を呼ぶ*/
+  });// ここまで画面が読み込まれたらすぐに動かしたい場合の記述
